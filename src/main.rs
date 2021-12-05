22720602ba979c9110d47ecb9d0248d765776860
Brian Faga <brian@accidentaldevelopment.com>
2021-12-05 10:36:53 -0500

    test: make all tests pass

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,71 @@ trait AsyncStockSignal {
     fn calculate(&self, series: &[f64]) -> Option<Self::SignalType>;
 }
 
+struct PriceDifference;
+struct MinPrice;
+struct MaxPrice;
+struct WindowedSMA {
+    window_size: usize,
+}
+
+impl AsyncStockSignal for PriceDifference {
+    type SignalType = (f64, f64);
+
+    fn calculate(&self, series: &[f64]) -> Option<Self::SignalType> {
+        if !series.is_empty() {
+            // unwrap is safe here even if first == last
+            let (first, last) = (series.first().unwrap(), series.last().unwrap());
+            let abs_diff = last - first;
+            let first = if *first == 0.0 { 1.0 } else { *first };
+            let rel_diff = abs_diff / first;
+            Some((abs_diff, rel_diff))
+        } else {
+            None
+        }
+    }
+}
+
+impl AsyncStockSignal for MinPrice {
+    type SignalType = f64;
+
+    fn calculate(&self, series: &[f64]) -> Option<Self::SignalType> {
+        if series.is_empty() {
+            None
+        } else {
+            Some(series.iter().fold(f64::MAX, |acc, q| acc.min(*q)))
+        }
+    }
+}
+
+impl AsyncStockSignal for MaxPrice {
+    type SignalType = f64;
+
+    fn calculate(&self, series: &[f64]) -> Option<Self::SignalType> {
+        if series.is_empty() {
+            None
+        } else {
+            Some(series.iter().fold(f64::MIN, |acc, q| acc.max(*q)))
+        }
+    }
+}
+
+impl AsyncStockSignal for WindowedSMA {
+    type SignalType = Vec<f64>;
+
+    fn calculate(&self, series: &[f64]) -> Option<Self::SignalType> {
+        if !series.is_empty() && self.window_size > 1 {
+            Some(
+                series
+                    .windows(self.window_size)
+                    .map(|w| w.iter().sum::<f64>() / w.len() as f64)
+                    .collect(),
+            )
+        } else {
+            None
+        }
+    }
+}
+
 ///
 /// Calculates the absolute and relative difference between the beginning and ending of an f64 series. The relative difference is relative to the beginning.
 ///
